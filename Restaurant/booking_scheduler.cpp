b55b2dc671a520c2b18519f0200304718234f2c4
uiyangco <jinsup13@naver.com>
2025-07-24 14:02:05 +0900

    mocking step 1

--- Restaurant/booking_scheduler.cpp
+++ Restaurant/booking_scheduler.cpp
@@ -35,14 +35,13 @@ public:
             throw std::runtime_error("Number of people is over restaurant capacity per hour");
         }
 
-        /*
+        
         // 일요일에는 시스템을 오픈하지 않는다.
-        time_t now = time(nullptr);
+        time_t now = getNow();
         if (getDayOfWeek(now) == "Sunday") {
             throw std::runtime_error("Booking system is not available on sunday");
         }
-        */
-
+        
         schedules.push_back(schedule);
 
         // 고객에게 SMS 발송
@@ -64,6 +63,11 @@ public:
 
     void setMailSender(MailSender* mailSender) {
         this->mailSender = mailSender;
+    }
+
+    virtual time_t getNow()
+    {
+        return time(nullptr);
     }
 
 private:
---
+++ Restaurant/testable_mail_sender.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "mail_sender.cpp"
+
+class TestableMailSender : public MailSender
+{
+public:
+	void sendMail(Schedule* schedule) override
+	{
+		countSendMailMethodIsCalled++;
+	}
+
+	int getCountSendMailMethodIsCalled()
+	{
+		return countSendMailMethodIsCalled;
+	}
+
+private:
+	int countSendMailMethodIsCalled = 0;
+};
